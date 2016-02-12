85b2efb0e59b75dab013ae5c3b836c308e9583ea
noodles <noodles_loves@yahoo.com.tw>
2016-02-12 23:08:49 +0800

    show

--- index.php
+++ index.php
@@ -2,17 +2,6 @@
 session_start();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by TEMPLATED
-http://templated.co
-Released for free under the Creative Commons Attribution License
-
-Name       : Breadth
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20140331
-
--->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -23,9 +12,11 @@ Released   : 20140331
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
-
-<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
-
+<link rel="stylesheet" type="text/css" href="/shadowbox/shadowbox.css"/>
+<script type="text/javascript" src="/shadowbox/shadowbox.js"></script>
+<script type="text/javascript">
+  Shadowbox.init();
+</script>
 </head>
 <body>
 
@@ -68,8 +59,8 @@ Released   : 20140331
       <div class="column2">
         <div>
           <ul>
-            <li><a href='https://www.youtube.com/watch?v=IhdhyvyExEA' target="_blank">part1_2</li>
-            <li><a href='https://www.youtube.com/watch?v=QkJkcCRT_6Y' target="_blank">part1_3</a></li>
+            <li><a href='https://www.youtube.com/watch?v=IhdhyvyExEA' target="_blank" rel="shadowbox">part1_2</li>
+            <li><a href='https://www.youtube.com/watch?v=QkJkcCRT_6Y' target="_blank" >part1_3</a></li>
           </ul>
         </div>
         <div class="title">
